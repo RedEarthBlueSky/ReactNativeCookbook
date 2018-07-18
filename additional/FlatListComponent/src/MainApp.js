7ae39ff47244e6fc50a41e2e71d1fc1d7e1981b8
Ian Salt <ian@isalt.digital>
2018-07-18 12:41:42 +0100

    makeRemoteRequestion function

--- additional/FlatListComponent/src/MainApp.js
+++ additional/FlatListComponent/src/MainApp.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import FlatListDemo from './FlatListDemo';
+
 
 class MainApp extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text style={styles.textStyle}>
           App to demo FlatList Component
         </Text>
+        <FlatListDemo />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   textStyle: {
     color: 'blue',
     fontSize: 20,
     fontWeight: 'bold',
-  }
+  },
 });
 
 export default MainApp;
