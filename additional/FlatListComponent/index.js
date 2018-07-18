79bde8c8caa706f34639cc2d651c9daa5e914f2f
Ian Salt <ian@isalt.digital>
2018-07-18 16:29:18 +0100

    deleted old project and new one functional needs styles

--- additional/FlatListComponent/index.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-import { AppRegistry } from 'react-native';
-import MainApp from './src/MainApp';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => MainApp);
