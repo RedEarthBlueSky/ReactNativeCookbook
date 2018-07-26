bb236c79923933cda1d13fa41b1783947f7ef05e
Ian Salt <ian@isalt.digital>
2018-07-26 11:33:05 +0100

    two animated and one static image completed

--- 3_Chapter/tip2MultipleAnimations/src/MainApp.js
+++ 3_Chapter/tip2MultipleAnimations/src/MainApp.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { View, Animated, Image, Easing, Dimensions, StyleSheet, Text } from 'react-native';
+import {
+  View,
+  Animated,
+  Image,
+  Easing,
+  Dimensions,
+  StyleSheet,
+  Text
+} from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 const cloudImage = require('./images/cloud.png');
@@ -28,29 +36,62 @@ class MainApp extends Component {
       }
     ).start(() => this.startAnimation());
   }
+
   render() {
     const left1 = this.animatedValue.interpolate({
       inputRange: [0, 1],
-      outputRange: [-cloudWidth, width]
+      outputRange: [-cloudWidth, width],
     });
 
     const left2 = this.animatedValue.interpolate({
       inputRange: [0, 1],
-      outputRange: [-cloudWidth * 5, width + (cloudWidth * 5)],
+      outputRange: [-cloudWidth*5, width + cloudWidth*5],
     });
 
     return (
       <View>
-        <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'red' }}>
-          Chapter 3, Tip 2: Multiple Animations
-        </Text>
+        <Animated.Image
+          style={[
+            styles.cloud1,
+            { left: left1 }
+          ]}
+          source={cloudImage}
+        />
+        <Animated.Image
+          style={[
+            styles.cloud1,
+            styles.cloud2,
+            { left: left2 }
+          ]}
+          source={cloudsImage}
+        />
+        <Image
+          style={[styles.cloud1, styles.plane]}
+          source={planeImage}
+        />
       </View>
     );
   }
 }
 
-styles = StyleSheet.create({
-  // something here
+const styles = StyleSheet.create({
+  cloud1: {
+    position: 'absolute',
+    width: cloudWidth,
+    height: cloudWidth,
+    top: (height / 3) - (cloudWidth / 2),
+  },
+  cloud2: {
+    width: cloudWidth * 1.3,
+    height: cloudWidth * 1.3,
+    top: height / 2,
+  },
+  plane: {
+    width: cloudWidth * 1.3,
+    height: cloudWidth * 1.3,
+    top: (height / 2) - cloudWidth,
+    left: (width / 2) - (cloudWidth / 2),
+  },
 });
 
 export default MainApp;
