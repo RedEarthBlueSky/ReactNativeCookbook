5b30830a1b511fae638b86c057c96ce13644580f
Ian Salt <ian@isalt.digital>
2018-07-31 09:52:08 +0100

    example completed

--- 4_Chapter/tip1SavingLocally/README.md
+++ 4_Chapter/tip1SavingLocally/README.md
@@ -1,2 +1,5 @@
 ## Store and retrieve data locally ##
 ### In theory, allow the user to keep using the app without and internet connection, store the data locally and then synch with the server when reconnected ###
+
+A nice example but data is not encrypted so not sure when you would use it unless
+you decided to encrypt the data.
--- 4_Chapter/tip1SavingLocally/src/MainApp.js
+++ 4_Chapter/tip1SavingLocally/src/MainApp.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import {
   Alert,
   AsyncStorage,
@@ -11,25 +11,67 @@ import {
 
 const key = '@MyApp:key';
 
-class MainApp extends Component {
+class MainApp extends PureComponent {
+  state = {
+    text: '',
+    storedValue: '',
+  };
+
+  componentWillMount() {
+    this.onLoad();
+  }
+
+  onChange = (text) => {
+    this.setState({ text });
+  }
+
+  onLoad = async () => {
+    try {
+      const storedValue = await AsyncStorage.getItem(key);
+      this.setState({ storedValue });
+    } catch (error) {
+      Alert.alert('Error', 'There was an error while loading the data');
+    }
+  }
+
+  onSave = async () => {
+    const { text } = this.state;
+
+    try {
+      await AsyncStorage.setItem(key, text);
+      Alert.alert('Saved', 'Successfully saved on device');
+    } catch (error) {
+      Alert.alert('Error', 'There was an error while saving the data');
+    }
+  }
+
   render() {
+    const { storedValue, text } = this.state;
+
     return (
-      <View>
-        <Text style={styles.title}>Ch 4: Tip 1 - Store and Retrieve Data Locally</Text>
+      <View style={styles.container}>
+        <Text style={styles.preview}>{storedValue}</Text>
+        <View>
+          <TextInput
+            style={styles.input}
+            onChangeText={this.onChange}
+            value={text}
+            placeholder='Type something here...'
+          />
+          <TouchableOpacity onPress={this.onSave} style={styles.btn}>
+            <Text>Save Locally</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.onLoad} style={styles.btn}>
+            <Text>Load Data</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  title: {
-    fontWeight: 'bold',
-    fontSize: 16,
-    color: '#fff',
-    backgroundColor: 'blue',
-    padding: 10,
-    marginTop: 20,
-  },
+
   container: {
     flex: 1,
     justifyContent: 'center',
