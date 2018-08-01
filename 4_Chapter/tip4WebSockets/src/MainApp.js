0babcb58f8283b5fe331261d5a1b87beeccffea3
Ian Salt <ian@isalt.digital>
2018-08-01 19:45:56 +0100

    completed

--- 4_Chapter/tip4WebSockets/src/MainApp.js
+++ 4_Chapter/tip4WebSockets/src/MainApp.js
@@ -11,10 +11,87 @@ import {
 const { width } = Dimensions.get('window');
 
 class MainApp extends Component {
-  render() {
+  state = {
+    history: [],
+  };
+
+  componentWillMount() {
+    this.ws = new WebSocket('ws://localhost:3001');
+
+    this.ws.onopen = this.onOpenConnection;
+    this.ws.onmessage = this.onMessageReceived;
+    this.ws.onerror = this.onError;
+    this.ws.onclose = this.onClose;
+    console.log('Component Did Mount');
+  }
+
+  onOpenConnection = () => {
+    console.log('Connection Open!');
+  }
+
+  onError = (event) => {
+    console.log('onerror', event.message);
+  }
+
+  onClose = (event) => {
+    console.log('onclose', event.code, event.reason);
+  }
+
+  onMessageReceived = (event) => {
+    this.setState({
+      history: [
+        ...this.state.history,
+        { owner: false, msg: event.data }
+      ],
+    });
+  }
+
+  onSendMessage = () => {
+    const { text } = this.state;
+
+    this.setState({
+      text: '',
+      history: [
+        ...this.state.history,
+        { owner: true, msg: text },
+      ],
+    });
+    this.ws.send(text);
+  }
+
+  onChangeText = (text) => {
+    this.setState({ text });
+  }
+
+  renderMessage(item, index) {
+    const kind = item.owner ? styles.me : styles.friend;
+
     return (
-      <View>
-        <Text style={styles.title}>Ch4 Tip4 - Web Sockets Real Time comms! </Text>
+      <View style={[styles.msg, kind]} key={index}>
+        <Text>{item.msg}</Text>
+      </View>
+    );
+  }
+
+  render() {
+    const { history, text } = this.state;
+    const { title, container, toolbar, content,
+      msg, inputContainer, input } = styles;
+
+    return (
+      <View style={container}>
+        <Text style={title}>Ch4 Tip4 - Web Sockets Real Time comms! </Text>
+        <ScrollView style={content}>
+          { history.map(this.renderMessage)}
+        </ScrollView>
+        <View style={inputContainer}>
+          <TextInput
+            style={input}
+            value={text}
+            onChangeText={this.onChangeText}
+            onSubmitEditing={this.onSendMessage}
+          />
+        </View>
       </View>
     );
   }
@@ -28,7 +105,44 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     padding: 10,
     marginTop: 30,
-  }
+  },
+  container: {
+    backgroundColor: '#ecf0f1',
+    flex: 1,
+  },
+  toolbar: {
+    backgroundColor: '#34495e',
+    color: '#fff',
+    fontSize: 20,
+    padding: 25,
+    textAlign: 'center',
+  },
+  content: {
+    flex: 1,
+  },
+  msg: {
+    margin: 5,
+    padding: 10,
+    borderRadius: 10,
+  },
+  me: {
+    alignSelf: 'flex-start',
+    backgroundColor: '#1abc9c',
+    marginRight: 100,
+  },
+  friend: {
+    alignSelf: 'flex-end',
+    backgroundColor: '#fff',
+    marginLeft: 100,
+  },
+  inputContainer: {
+    backgroundColor: '#bdc3c7',
+    padding: 5,
+  },
+  input: {
+    height: 40,
+    backgroundColor: '#fff',
+  },
 });
 
 export default MainApp;
