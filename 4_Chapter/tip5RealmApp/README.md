fdc11ad367dd0833ea0eb6026757f91431ea9526
Ian Salt <ian@isalt.digital>
2018-08-03 12:13:12 +0100

    completed in single MainApp.js file

--- 4_Chapter/tip5RealmApp/README.md
+++ 4_Chapter/tip5RealmApp/README.md
@@ -1,4 +1,8 @@
-##Chapter 4 Tip 5 Real App ##
+##Chapter 4 Tip 5 Realm App ##
 ### Realm is a fast, thread-safe, transactional, object-based database primarily designed for use by mobile devices, with a JavaScript UI.  It also supports encryption, complex querying, UI bindings and more. ###
 
 #### Here we will create a simple database and perform basic operations such as inserting records, updating and deleting.  Most importantly, we will display the records of our schema on the UI. ####
+
+####
+Unlike the book, you can create the code in a single App file and you will not have an issue in loading in either Android or iOS
+####
--- 4_Chapter/tip5RealmApp/src/MainApp.js
+++ 4_Chapter/tip5RealmApp/src/MainApp.js
@@ -5,15 +5,18 @@ import {
   Text,
 } from 'react-native';
 import Realm from 'realm';
-// import Button from 'react-native-button';
+import Button from 'react-native-button';
+
+const api = 'https://randomuser.me/api/';
 
 class MainApp extends Component {
+  realm: undefined
 
   componentWillMount() {
     const realm = this.realm = new Realm({
       schema: [
         {
-          name: 'user',
+          name: 'User',
           properties: {
             firstName: 'string',
             lastName: 'string',
@@ -25,27 +28,107 @@ class MainApp extends Component {
     this.setState({ users: realm.objects('User') });
   }
 
-  realm: undefined
+  getRandomUser() {
+    return fetch(api).then((response) => response.json());
+  }
 
-  
+  //  create a user object
+  createUser = () => {
+    const realm = this.realm;
+
+    this.getRandomUser().then((response) => {
+      const user = response.results[0];
+      const userName = user.name;
+      realm.write(() => {
+        realm.create('User', {
+          firstName: userName.first,
+          lastName: userName.last,
+          email: user.email,
+        });
+        this.setState({ users: realm.objects('User') });
+      });
+    });
+  }
+
+  updateUser = () => {
+    const realm = this.realm;
+    const users = realm.objects('User');
+
+    realm.write(() => {
+      if (users.length) {
+        let firstUser = users.slice(0, 1)[0];
+        firstUser.firstName = 'Bob';
+        firstUser.lastName = 'Cookbook';
+        firstUser.email = 'react.native@cookbook.com';
+        this.setState(users);
+      }
+    });
+  }
+
+  deleteUsers = () => {
+    const realm = this.realm;
+
+    realm.write(() => {
+      realm.deleteAll();
+      this.setState({ users: realm.objects('User') });
+    });
+  }
 
   render() {
     return (
-      <View>
-        <Text style={styles.title}>Ch4 Tip5 Realm Database App</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to the Realm DB Test</Text>
+        <View style={styles.container}>
+          <Button onPress={this.createUser}>
+            Add User
+          </Button>
+          <Button onPress={this.updateUser}>
+            Update First User
+          </Button>
+          <Button onPress={this.deleteUsers}>
+            Remove All Users
+          </Button>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Users: </Text>
+        {
+          this.state.users.map((user, idx) => {
+            return (<Text key={idx}>
+                {user.firstName} {user.lastName} {user.email}
+              </Text>);
+          })
+        }
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  title: {
-    backgroundColor: 'blue',
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginTop: 30,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    paddingTop: 25
+  },
+  buttonContainer: {
+    width: 200,
     padding: 10,
+    margin: 5,
+    height: 40,
+    overflow: 'hidden',
+    borderRadius: 4,
+    backgroundColor: '#FF5722'
+  },
+  buttonStyle: {
+    fontSize: 16,
+    color: 'white'
   },
 });
 
